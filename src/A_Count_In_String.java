f8857fd063f65b533c12336934582aa14c538a9e
tripti <triptilakhina9286@gmail.com>
2023-04-04 09:17:28 +0100

    count of 'a' in a user input string commit

---
+++ src/A_Count_In_String.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class A_Count_In_String {
+    Scanner scanner=new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        A_Count_In_String a_count_in_string = new A_Count_In_String();
+        a_count_in_string.countTotalNumberOfA();
+    }
+
+
+
+    public void countTotalNumberOfA(){
+        System.out.println("Enter any word/s or sentence/s");
+        String sentence=scanner.nextLine();
+        int count=0;
+        int charCount = sentence.length() - sentence.replaceAll("a", "").length();
+
+        for (int i = 0; i < charCount; i++) {
+            if (sentence.charAt(i) != ' ')
+            count++;
+
+
+        } System.out.println("Total number of character 'a' in your string" + " \"" + sentence + "\" "+ "are: "+ count);
+    }
+}
